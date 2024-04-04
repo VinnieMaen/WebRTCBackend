541b4032219ad45772e5c229aeb5986f73d04b98
vincent <vincent_maenhout@hotmail.com>
2024-04-04 18:05:31 +0200

    Added database + socket connections and Models

--- index.ts
+++ index.ts
@@ -1,16 +1,36 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import { init } from "./lib/socketio";
+import { ObjectId } from "mongoose";
+import { connect } from "./lib/database";
+import api from "./routes";
+import cors from "cors";
 
 //For env File
 dotenv.config();
 
+declare global {
+  namespace Express {
+    interface Request {
+      customData: string;
+      id: ObjectId;
+    }
+  }
+}
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
+const server = require("http").createServer(app);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
-});
+app.use(express.json());
 
-app.listen(port, () => {
+app.use(cors());
+connect();
+
+app.use("/api/", api);
+
+server.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
+
+init(server);
